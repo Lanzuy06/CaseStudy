23fe48136be0b9e6d41263dfcbb7908c7566f8b0
Lanw Williaw Uy <Lanzuy06@gmail.com>
2021-11-08 14:00:10 +0800

    Case study

---
+++ clinicalsapiLanz/src/main/java/com/lanz/clinicals/ClinicalsapiLanzApplication.java
@@ -0,0 +1,13 @@
+package com.lanz.clinicals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicalsapiLanzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicalsapiLanzApplication.class, args);
+	}
+
+}
---
+++ clinicalsapiLanz/src/test/java/com/lanz/clinicals/ClinicalsapiLanzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lanz.clinicals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicalsapiLanzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
